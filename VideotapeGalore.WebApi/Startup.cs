b28d849831abeecac6a32d6838db71f8935e0ddd
Fanarito <viktor.saevars@gmail.com>
2018-10-22 10:49:55 +0000

    Added db to webapi

--- VideotapeGalore.WebApi/Startup.cs
+++ VideotapeGalore.WebApi/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using VideotapeGalore.Repositories;
 
 namespace VideotapeGalore.WebApi
 {
@@ -26,6 +28,11 @@ namespace VideotapeGalore.WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            
+            // Add application db
+            var connectionString = Configuration.GetConnectionString("Application");
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlite(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
